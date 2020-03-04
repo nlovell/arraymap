faa81f18b29325e2f94f0e519c221f105b2b8293
Alan Lovell <git@nlovell.net>
2020-03-04 16:41:43 +0000

    Update catch2_test.cpp

--- arraymap-cpp/catch2_test.cpp
+++ arraymap-cpp/catch2_test.cpp
@@ -3,13 +3,87 @@
 #include "Keymap.h"
 
 /************************************************************************************
-	A class to manually test the map.
-
+	A class to automatically test the map.
+	
 	Designed and implemented by Student 20107104674840, November 2019.
+	
         Depends on the Catch2 Test framework. 
         Catch2 can be found here: https://github.com/catchorg/Catch2/
         The catch.hpp file can be found at https://tinyurl.com/ussa7h6
 *************************************************************************************/
+
+TEST_CASE("Adding 100 integers", "[int:int]")
+{
+    Keymap<int, int> *km = new Keymap<int, int>;
+    int var = 100;
+
+    for (int i = 0; i <= var; i++)
+    {
+        std::cout << i << ",";
+        REQUIRE(km->insert(i, i) == true);
+    }
+
+    REQUIRE(km->getLength() >= var);
+
+    for (int i = 0; i <= var; i++)
+    {
+        REQUIRE(km->removePair(i) == true);
+    }
+
+    REQUIRE(km->getLength() == 0);
+    REQUIRE(km->getCapacity() < 5);
+
+    km->reset();
+    REQUIRE(km->getCapacity() == 1);
+    REQUIRE(km->getLength() == 0);
+    REQUIRE(km->isEmpty());
+}
+
+TEST_CASE("Keymap with bool:double", "[bool:double]")
+{
+    Keymap<bool, double> *km = new Keymap<bool, double>;
+    REQUIRE(km->getLength() == 0);
+    REQUIRE(km->isEmpty());
+
+    int oldCap = km->getCapacity();
+    REQUIRE(oldCap < km->enlargeMap());
+
+    REQUIRE(km->insert(true, 100.01) == true);
+    REQUIRE(km->getValue(true) == 100.01);
+    REQUIRE(km->keyExists(true) == true);
+    REQUIRE(km->keyExists(false) == false);
+
+    REQUIRE(km->insert(true, 200.01) == false);
+    REQUIRE(km->getValue(true) == 100.01);
+    REQUIRE(km->getIndex(true) <= km->getCapacity());
+
+    REQUIRE(km->getLength() == 1);
+    REQUIRE(km->removePair(true) == true);
+    REQUIRE(km->getLength() == 0);
+
+    km->reset();
+    REQUIRE(km->getCapacity() == 1);
+    REQUIRE(km->getLength() == 0);
+    REQUIRE(km->isEmpty());
+
+    REQUIRE(km->insertOrUpdate(true, 100.01) == true);
+    REQUIRE(km->getValue(true) == 100.01);
+
+    REQUIRE(km->insertOrUpdate(false, 200.01) == true);
+    REQUIRE(km->getValue(false) == 200.01);
+
+    REQUIRE(km->keyExists(true) == true);
+    REQUIRE(km->keyExists(false) == true);
+
+    REQUIRE(km->insertOrUpdate(true, 1300.01) == false);
+    REQUIRE(km->getValue(true) == 1300.01);
+
+    REQUIRE(km->removePair(true) == true);
+    REQUIRE(km->keyExists(true) == false);
+    REQUIRE(km->removePair(true) == false);
+
+    REQUIRE(km->getIndex(false) <= km->getCapacity());
+};
 
 TEST_CASE("Keymaps with Int:Chars", "[int:char]")
 {
@@ -50,6 +124,56 @@ TEST_CASE("Keymaps with Int:Chars", "[int:char]")
     km->removePair(1);
     REQUIRE(km->getValue(2) == 'b');
     REQUIRE(km->getValue(3) == 'c');
+    REQUIRE(km->getLength() == 2);
+    REQUIRE(km->getCapacity() >= km->getLength());
+
+    km->reset();
+    REQUIRE(km->getCapacity() == 1);
+    REQUIRE(km->getLength() == 0);
+    REQUIRE(km->isEmpty());
+
+    km->~Keymap();
+};
+
+TEST_CASE("Keymaps with Float:Strings", "[int:string]")
+{
+    Keymap<float, std::string> *km = new Keymap<float, std::string>;
+    REQUIRE(km->getLength() == 0);
+    REQUIRE(km->isEmpty());
+
+    int cap1 = km->getCapacity();
+    km->enlargeMap();
+    int cap2 = km->getCapacity();
+    REQUIRE(cap1 < cap2);
+
+    REQUIRE(km->insert(1.1, "foo") == true);
+    REQUIRE(km->getValue(1.1) == "foo");
+
+    REQUIRE(km->insert(1.1, "Foo") == false);
+    REQUIRE(km->getValue(1.1) == "foo");
+
+    km->updatePair(1.1, "FOO");
+    REQUIRE(km->getValue(1.1) == "FOO");
+
+    REQUIRE(km->insertOrUpdate(1.1, "foo") == false);
+    REQUIRE(km->getValue(1.1) == "foo");
+
+    REQUIRE(km->insertOrUpdate(22.2, "bar") == true);
+    REQUIRE(km->getValue(1.1) == "foo");
+    REQUIRE(km->getValue(22.2) == "bar");
+    REQUIRE(km->getLength() == 2);
+    REQUIRE(km->getCapacity() >= km->getLength());
+
+    km->insert(333.3, "baz");
+    REQUIRE(km->getValue(1.1) == "foo");
+    REQUIRE(km->getValue(22.2) == "bar");
+    REQUIRE(km->getValue(333.3) == "baz");
+    REQUIRE(km->getLength() == 3);
+    REQUIRE(km->getCapacity() >= km->getLength());
+
+    km->removePair(1.1);
+    REQUIRE(km->getValue(22.2) == "bar");
+    REQUIRE(km->getValue(333.3) == "baz");
     REQUIRE(km->getLength() == 2);
     REQUIRE(km->getCapacity() >= km->getLength());
 
